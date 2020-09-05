69752d4df859fab0cc82967ebefb2cccb0b697f2
Burak <burak@MacBook-Pro.local>
2020-09-06 01:33:16 +0300

    Alertifyjs, Bootswatch implemented.

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
   baseUrl = "http://localhost:5000/api/auth/";
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
   constructor(private http: HttpClient) {}
 
   login(model: any) {
     return this.http.post(this.baseUrl + "login", model).pipe(
       map((response: any) => {
-        debugger;
         const userObject = response;
         if (userObject) {
+          debugger;
           localStorage.setItem("token", userObject.token);
+          this.decodedToken = this.jwtHelper.decodeToken(userObject.token);
+          console.log(this.decodedToken);
         }
       })
     );
@@ -24,5 +29,10 @@ export class AuthService {
 
   register(model: any) {
     return this.http.post(this.baseUrl + "register", model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem("token");
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,35 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http'
-import {FormsModule} from '@angular/forms'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { BsDropdownModule } from "ngx-bootstrap";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { AuthService } from './_services/auth.service';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { AuthService } from "./_services/auth.service";
+import { RegisterComponent } from "./register/register.component";
+import { HomeComponent } from "./home/home.component";
+import { ErrorInterceptorProvider } from "./_services/error.interceptor";
 
 @NgModule({
-   declarations: [			
-      AppComponent,
-      NavComponent,
-      RegisterComponent,
-      HomeComponent
-   ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule,
-      HttpClientModule,
-      FormsModule
-   ],
-   providers: [
-      AuthService,
-      ErrorInterceptorProvider
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, NavComponent, RegisterComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+  ],
+  providers: [AuthService, ErrorInterceptorProvider],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
